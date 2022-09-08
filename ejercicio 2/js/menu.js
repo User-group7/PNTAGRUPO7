09f816b9d2a5a81af8ad5a0be471ce7b42a7dbea
User-group7 <111472517+User-group7@users.noreply.github.com>
2022-09-08 00:04:33 -0300

    Delete ejercicio 2 directory

--- ejercicio 2/js/menu.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", () => {
-    let navbar = document.getElementById("navbar");
-    var value = window.scrollY;
-    if(value >= 700) {
-      navbar.className = "menu menu-opacity";
-    } else {
-      navbar.className = "menu";
-    }
-  });

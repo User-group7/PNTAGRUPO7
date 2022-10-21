fef3978e0d38e0091767a4edea25062c418f4f6c
User-group7 <111472517+User-group7@users.noreply.github.com>
2022-10-21 17:13:52 -0300

    Add files via upload

---
+++ HardwareSIST/HardwareSIST/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HardwareSIST
+{
+    public class Product
+    {
+        public Product()
+        {
+        }
+
+        //CONSTRUCTOR
+        public Product(int productId, Category catId, String productName, String modelNo, String productDescription)
+        {
+            ProductId = productId;
+            CatId = catId;
+            ProductName = productName;
+            ModelNo = modelNo;
+            ProductDescription = productDescription;
+        }
+
+        public int ProductId { get; set; }
+        public Category CatId { get; set; }
+        public String ProductName { get; set; }
+        public String ModelNo { get; set; }
+        public String ProductDescription { get; set; }
+
+
+        public String InfoProduct()
+        {
+            return "Nombre: " + ProductName +
+                    "Id: " + ProductId;
+        }
+    }
+}
---
+++ HardwareSIST/HardwareSIST/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareSIST
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //creo la primer categoría
+            Category category1 = new Category("C1-LAP", "LAPTOPS");
+            Product product1 = new Product(001, category1, "LaptopEjemplo", "Modelo1", "LaptopDeEjemploD");
+            Product product2 = new Product(002, category1, "LaptopEjemplo2", "Modelo2", "LaptopDeEjemploD");
+
+            ProductRepostory entregados = new ProductRepostory("entregados");
+            entregados.addProduct(product1);
+
+            ProductRepostory recibidos = new ProductRepostory("recibidos");
+            recibidos.addProduct(product2);
+            //creo una lista de productos
+            //List<Product> ListaProducts = new List<Product>()
+            //{
+            //  new Product(001, category1, "LaptopEjemplo", "Modelo1", "LaptopDeEjemploD"),
+            // new Product(002, category1, "LaptopEjemplo2", "Modelo2", "LaptopDeEjemploD")
+            //};
+            recibidos.showProducts();
+        
+        }
+    }
+}
